d82f0f378e9f17fe4da9971a83f5692059ad06a3
shestakov2103 <75324832+shestakov2103@users.noreply.github.com>
2020-12-01 21:15:16 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,48 @@
 <?php
 
-echo 'geekbrains';
+class Offer
+{
+	private $title;
+	private $price;
+
+	public function __construct($title, $price)
+	{
+		$this->title = $title;
+		$this->price = $price;
+	}
+
+	public function getTitle()
+	{
+		return $this->title;
+	}
+
+	public function setTitle($title)
+	{
+		$this->title = $title;
+	}
+
+	public function getPrice()
+	{
+		return $this->price;
+	}
+
+	public function setPrice($price)
+	{
+		$this->price = $price;
+	}
+}
+
+class OfferDiscount extends Offer
+{
+	protected $discount;
+
+	public function getDiscount()
+	{
+		return $this->discount;
+	}
+
+	public function setDiscount($discount)
+	{
+		$this->discount = $discount;
+	}
+}
